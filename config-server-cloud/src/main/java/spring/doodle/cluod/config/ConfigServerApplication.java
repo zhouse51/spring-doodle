e91a8126ab08d3c767a8dcbc3048d395e3aabd13
James Zhou <zhouqi@jamess-mbp.lan>
2019-02-12 14:56:07 -0600

    control servers
    including registrator server, config server (cluster support), and client supports

--- config-server-cloud/src/main/java/spring/doodle/cluod/config/ConfigServerApplication.java
+++ config-server-cloud/src/main/java/spring/doodle/cluod/config/ConfigServerApplication.java
@@ -1,18 +1,18 @@
-package spring.doodle.cloud.name;
+package spring.doodle.cluod.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigurationProperties(NameProperties.class)
+@EnableConfigServer
 @EnableDiscoveryClient
-public class NameApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NameApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
-	
+
 }
 
